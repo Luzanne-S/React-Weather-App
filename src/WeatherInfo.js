6cdd48988bffb94ac1be4e8413c9f6b6c2abbf9a
Luzanne <luzannejss@gmail.com>
2021-08-01 13:34:32 +0200

    Updated WeatherInfo
    Added file outline

---
+++ src/WeatherInfo.js
@@ -0,0 +1,33 @@
+import React from "react";
+import WeatherDate from "./WeatherDate";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <div className="container2">
+        <div className="row">
+          <div className="col">
+            <ul>
+              <li>
+                <WeatherDate date={props.data.date} />
+              </li>
+              <li className="description">{props.data.description}</li>
+              <li>Humidity: {props.data.humidity}</li>
+              <li>Wind: {props.data.wind}</li>
+            </ul>
+          </div>
+          <div className="col1">
+            <p className="temperature" />
+            <strong>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </strong>
+            <span className="units">°C | °F</span>{" "}
+            <span className="icon">{props.data.icon}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
