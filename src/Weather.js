5ae5d98bd9fd6adf802bd2901e6fa219ddbcf500
Luzanne <luzannejss@gmail.com>
2021-08-01 13:33:07 +0200

    Updated Weather.js
    Imported Axios and WeatherInfo file
    Added functions
    Weather data added

--- src/Weather.js
+++ src/Weather.js
@@ -1,69 +1,76 @@
-import React from "react";
-import "axios";
-import "./WeatherInfo";
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
 import "./App.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Johannesburg",
-    temperature: 13,
-    date: "Friday 17:56",
-    description: "Light Showers",
-    humidity: "3%",
-    wind: "10km",
-    imgUrl: " http://openweathermap.org/img/wn/10d@2x.png",
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  return (
-    <div className="Weather">
-      <div className="card">
-        <form className="form-inline my-2 my-lg-0 searchBar">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search city"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-secondary my-2 my-sm-0"
-            type="submit"
-          >
-            <svg
-              className="icon"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-            </svg>
-          </button>
-        </form>
-        <h1>{weatherData.city}</h1>
-        <div className="container2">
-          <div className="row">
-            <div className="col">
-              <ul>
-                <li className="date">{weatherData.date}</li>
-                <li className="description">{weatherData.description}</li>
-                <li>Humidity: {weatherData.humidity}</li>
-                <li>Wind: {weatherData.wind}</li>
-              </ul>
-            </div>
-            <div className="col1">
-              <p className="temperature" />
-              <strong>{weatherData.temperature}</strong>
-              <span className="units">°C | °F</span>{" "}
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                className="img"
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      date: new Date(response.data.dt * 1000),
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: response.data.weather[0].icon,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "4ad046b76be74520a0cad560d5d143a8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="card">
+          <form onSubmit={handleSubmit}>
+            <div className="form-inline my-2 my-lg-0 searchBar">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search city"
+                aria-label="Search"
+                onChange={handleCityChange}
               />
+              <button
+                className="btn btn-outline-secondary my-2 my-sm-0"
+                type="submit"
+                value="Search"
+              >
+                <svg
+                  className="icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+              </button>
             </div>
-          </div>
+          </form>
+          <WeatherInfo data={weatherData} />
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
